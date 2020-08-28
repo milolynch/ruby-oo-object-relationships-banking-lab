a10ef25a448d9fa9b7b889360e8d8856f3ddcc3a
Milo_Lynch <Milobravman20@gmail.com>
2020-08-28 14:42:43 -0400

    The start of the lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
 
+  @@all_accounts = []
+
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @status = "open"
+    @balance = 1000
+    @@all_accounts << self
+  end
+
+  def deposit(num)
+    self.balance = self.balance + num
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    
+  end
+
 end
